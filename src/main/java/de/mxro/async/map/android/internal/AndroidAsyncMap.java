5b3bb0ed28b3c99542f52abc17538f1e049f7a25
mxro <mxro@nowhere.com>
2014-08-14 08:13:05 +1000

    Improving way services are stopped. Adding utility to look for pending requests Autocommit Thu Aug 14 08:13:05 EST 2014

--- src/main/java/de/mxro/async/map/android/internal/AndroidAsyncMap.java
+++ src/main/java/de/mxro/async/map/android/internal/AndroidAsyncMap.java
@@ -1,5 +1,8 @@
 package de.mxro.async.map.android.internal;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import de.mxro.async.callbacks.SimpleCallback;
@@ -8,32 +11,33 @@ import de.mxro.async.map.AsyncMap;
 import de.mxro.async.map.android.SQLiteConfiguration;
 import de.mxro.async.map.operations.MapOperation;
 import de.mxro.serialization.Serializer;
+import de.mxro.serialization.jre.SerializationJre;
 import de.mxro.serialization.jre.StreamDestination;
 import de.mxro.serialization.jre.StreamSource;
 
 public class AndroidAsyncMap<V> implements AsyncMap<String, V> {
 
 	private final SQLiteConfiguration conf;
-	
+
 	private final SQLiteDatabase db;
 	private final Serializer<StreamSource, StreamDestination> serializer;
-	
+
 	@Override
 	public void put(String key, V value, SimpleCallback callback) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void get(String key, ValueCallback<V> callback) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void remove(String key, SimpleCallback callback) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -44,40 +48,51 @@ public class AndroidAsyncMap<V> implements AsyncMap<String, V> {
 
 	@Override
 	public void putSync(String key, V value) {
-		 final ContentValues cv = new ContentValues();
-		 
-		 cv.put(conf.getKeyColumnName(), k);
-		 
+		final ContentValues cv = new ContentValues();
+
+		cv.put(conf.getKeyColumnName(), key);
+
+		ByteArrayOutputStream os = new ByteArrayOutputStream(1024);
+		serializer.serialize(value,
+				SerializationJre.createStreamDestination(os));
+
+		cv.put(key, os.toByteArray());
+		
+		try {
+			os.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public void removeSync(String key) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void start(SimpleCallback callback) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void stop(SimpleCallback callback) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void commit(SimpleCallback callback) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void performOperation(MapOperation operation) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
