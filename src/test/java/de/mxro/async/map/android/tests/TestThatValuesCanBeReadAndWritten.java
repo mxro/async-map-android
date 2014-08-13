efba2576c4b11fd1c12d14f19b97a265e9f7720f
mxro <mxro@nowhere.com>
2014-08-14 09:47:06 +1000

    Testing first simple implementation and resolving issues Autocommit Thu Aug 14 09:47:06 EST 2014

--- src/test/java/de/mxro/async/map/android/tests/TestThatValuesCanBeReadAndWritten.java
+++ src/test/java/de/mxro/async/map/android/tests/TestThatValuesCanBeReadAndWritten.java
@@ -5,8 +5,6 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
-import android.app.Activity;
-
 @RunWith(RobolectricTestRunner.class)
 public class TestThatValuesCanBeReadAndWritten {
 
@@ -14,6 +12,10 @@ public class TestThatValuesCanBeReadAndWritten {
 	@Test
 	  public void clickingButton_shouldChangeResultsViewText() throws Exception {
 	    TestActivity activity = Robolectric.buildActivity(TestActivity.class).create().get();
+	    
+	    
+	    
+	    
 	}
 	
 }
