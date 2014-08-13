14eefda5236c3471d6eeee5d8b353e80ac6dca40
mxro <mxro@nowhere.com>
2014-08-14 08:28:05 +1000

    Implementing Android version of async-map Autocommit Thu Aug 14 08:28:05 EST 2014

--- src/main/java/de/mxro/async/map/android/SQLiteConfiguration.java
+++ src/main/java/de/mxro/async/map/android/SQLiteConfiguration.java
@@ -2,6 +2,8 @@ package de.mxro.async.map.android;
 
 public interface SQLiteConfiguration {
 
+	public String getDatabasePath();
+	
 	public String getTableName();
 	
 	public String getKeyColumnName();
