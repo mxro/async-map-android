156e4cedc18f6728332896ad8d005abd132371f2
mxro <mxro@nowhere.com>
2014-08-14 09:48:06 +1000

    Testing first simple implementation and resolving issues Autocommit Thu Aug 14 09:48:06 EST 2014

--- src/main/java/de/mxro/async/map/android/AsyncMapAndorid.java
+++ src/main/java/de/mxro/async/map/android/AsyncMapAndorid.java
@@ -1,5 +1,16 @@
 package de.mxro.async.map.android;
 
+import de.mxro.async.map.AsyncMap;
+import de.mxro.async.map.android.internal.AndroidAsyncMap;
+import de.mxro.serialization.Serializer;
+import de.mxro.serialization.jre.StreamDestination;
+import de.mxro.serialization.jre.StreamSource;
+
 public class AsyncMapAndorid {
+
+	public static <V> AsyncMap<String, V> createMap(SQLiteConfiguration conf,
+			Serializer<StreamSource, StreamDestination> serializer) {
+		return new AndroidAsyncMap<V>(conf, serializer);
+	}
 
 }
