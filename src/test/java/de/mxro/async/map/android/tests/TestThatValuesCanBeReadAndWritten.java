4be9d9df5f66c0e07cf9de1fa4c5996d717c6b91
mxro <mxro@nowhere.com>
2014-08-14 10:11:06 +1000

    Testing first simple implementation and resolving issues Autocommit Thu Aug 14 10:11:06 EST 2014

--- src/test/java/de/mxro/async/map/android/tests/TestThatValuesCanBeReadAndWritten.java
+++ src/test/java/de/mxro/async/map/android/tests/TestThatValuesCanBeReadAndWritten.java
@@ -4,34 +4,47 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowSQLiteDatabase;
 
+import de.mxro.async.Async;
+import de.mxro.async.Deferred;
+import de.mxro.async.callbacks.ValueCallback;
+import de.mxro.async.jre.AsyncJre;
 import de.mxro.async.map.AsyncMap;
 import de.mxro.async.map.android.AsyncMapAndorid;
 import de.mxro.async.map.android.SQLiteConfiguration;
+import de.mxro.fn.Success;
 import de.mxro.serialization.jre.SerializationJre;
 
 @RunWith(RobolectricTestRunner.class)
+@Config(manifest=Config.NONE)
 public class TestThatValuesCanBeReadAndWritten {
 
 	@Test
 	public void test() throws Exception {
 
 		SQLiteConfiguration conf = AsyncMapAndorid.createDefaultConfiguration();
-		AsyncMap<String, Object> map = AsyncMapAndorid.createMap(conf,
+		final AsyncMap<String, Object> map = AsyncMapAndorid.createMap(conf,
 				SerializationJre.newJavaSerializer(),
 				ShadowSQLiteDatabase.create(null));
 
 		map.putSync("one", 1);
 
 		map.putSync("two", 2);
-		
+
 		Assert.assertEquals(1, map.getSync("one"));
 		Assert.assertEquals(2, map.getSync("two"));
-		
-		
+
+		AsyncJre.waitFor(new Deferred<Success>() {
+
+			@Override
+			public void get(ValueCallback<Success> callback) {
+				System.out.println(map.getSync("two"));
+				map.stop(Async.wrap(callback));
+			}
+		});
 
 	}
 }
