8856d8989117f45fefa1b0232abfd492fac6bcf2
mxro <mxro@nowhere.com>
2014-08-14 10:09:06 +1000

    Testing first simple implementation and resolving issues Autocommit Thu Aug 14 10:09:06 EST 2014

--- src/test/java/de/mxro/async/map/android/tests/TestThatValuesCanBeReadAndWritten.java
+++ src/test/java/de/mxro/async/map/android/tests/TestThatValuesCanBeReadAndWritten.java
@@ -14,18 +14,19 @@ import de.mxro.serialization.jre.SerializationJre;
 @RunWith(RobolectricTestRunner.class)
 public class TestThatValuesCanBeReadAndWritten {
 
-	
 	@Test
-	  public void clickingButton_shouldChangeResultsViewText() throws Exception {
-	    TestActivity activity = Robolectric.buildActivity(TestActivity.class).create().get();
-	    
-	    
-	    SQLiteConfiguration conf = AsyncMapAndorid.createDefaultConfiguration(); 
-		AsyncMap<String, Object> map = AsyncMapAndorid.createMap(conf, SerializationJre.newJavaSerializer(),  ShadowSQLiteDatabase.create(null));
-	    		
-		
+	public void test() throws Exception {
+		TestActivity activity = Robolectric.buildActivity(TestActivity.class)
+				.create().get();
 
-	    
+		SQLiteConfiguration conf = AsyncMapAndorid.createDefaultConfiguration();
+		AsyncMap<String, Object> map = AsyncMapAndorid.createMap(conf,
+				SerializationJre.newJavaSerializer(),
+				ShadowSQLiteDatabase.create(null));
+
+		map.putSync("one", 1);
+
+		map.putSync("two", 2);
+
 	}
-	
 }
