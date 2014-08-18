edc9d69aa06a765c9e82724fe58a5ac13f1d640d
mxro <mxro@nowhere.com>
2014-08-19 08:33:40 +1000

    Optimize proguard configuration Autocommit Tue Aug 19 08:33:40 EST 2014

--- src/main/java/de/mxro/async/map/android/internal/AndroidAsyncMap.java
+++ src/main/java/de/mxro/async/map/android/internal/AndroidAsyncMap.java
@@ -21,6 +21,8 @@ import de.mxro.serialization.jre.StreamSource;
 
 public class AndroidAsyncMap<V> implements AsyncMap<String, V> {
 
+	private final boolean ENABLE_LOG = true;
+
 	private final SQLiteConfiguration conf;
 
 	private final Serializer<StreamSource, StreamDestination> serializer;
@@ -53,7 +55,7 @@ public class AndroidAsyncMap<V> implements AsyncMap<String, V> {
 	@SuppressWarnings("unchecked")
 	@Override
 	public V getSync(String key) {
-		
+
 		byte[] data = executeQueryImmidiately(createSelectStatement(), key);
 
 		if (data == null) {
@@ -66,7 +68,6 @@ public class AndroidAsyncMap<V> implements AsyncMap<String, V> {
 		return (V) object;
 	}
 
-	
 	private String createSelectStatement() {
 		final String sql = "SELECT " + conf.getKeyColumnName() + ", "
 				+ conf.getValueColumnName() + " FROM " + conf.getTableName()
@@ -78,9 +79,11 @@ public class AndroidAsyncMap<V> implements AsyncMap<String, V> {
 	@Override
 	public void putSync(String key, V value) {
 		assert key.length() <= AsyncMapAndorid.KEY_LENGTH;
-		
-		System.out.println(this+": Put "+key+" Value "+value);
-		
+
+		if (ENABLE_LOG) {
+			System.out.println(this + ": Put " + key + " Value " + value);
+		}
+
 		SQLiteStatement statement = createInsertStatement(key, value);
 
 		executeInsertStatementImmidiately(statement);
@@ -88,35 +91,37 @@ public class AndroidAsyncMap<V> implements AsyncMap<String, V> {
 	}
 
 	private byte[] executeQueryImmidiately(String sql, String key) {
-		
+
 		Cursor query = db.query(
 				conf.getTableName(),
 				new String[] { conf.getKeyColumnName(),
 						conf.getValueColumnName() }, conf.getKeyColumnName()
-						+ "=?", new String[] { key }, null, null, null );
+						+ "=?", new String[] { key }, null, null, null);
 		if (query.getCount() == 0) {
 			return null;
 		}
-		
+
 		query.moveToFirst();
 		byte[] data = query.getBlob(1);
 		query.close();
 		return data;
 	}
 
-	private void executeUpdateOrDeleteStatementImmidiately(SQLiteStatement statement) {
+	private void executeUpdateOrDeleteStatementImmidiately(
+			SQLiteStatement statement) {
 		db.beginTransaction();
 
 		int rowsAffected = statement.executeUpdateDelete();
 
 		if (rowsAffected != 1) {
-			throw new RuntimeException("No rows could be found for query "+statement.toString());
+			throw new RuntimeException("No rows could be found for query "
+					+ statement.toString());
 		}
-		
+
 		db.setTransactionSuccessful();
 		db.endTransaction();
 	}
-	
+
 	private void executeInsertStatementImmidiately(SQLiteStatement statement) {
 		db.beginTransaction();
 
@@ -127,7 +132,9 @@ public class AndroidAsyncMap<V> implements AsyncMap<String, V> {
 	}
 
 	private SQLiteStatement createInsertStatement(String key, V value) {
-		String sql = "INSERT OR REPLACE INTO " + conf.getTableName() + " ("+conf.getKeyColumnName()+","+conf.getValueColumnName()+") VALUES (?, ?)";
+		String sql = "INSERT OR REPLACE INTO " + conf.getTableName() + " ("
+				+ conf.getKeyColumnName() + "," + conf.getValueColumnName()
+				+ ") VALUES (?, ?)";
 		SQLiteStatement statement = db.compileStatement(sql);
 
 		statement.bindString(1, key);
@@ -142,7 +149,7 @@ public class AndroidAsyncMap<V> implements AsyncMap<String, V> {
 
 	@Override
 	public void removeSync(String key) {
-		
+
 		executeUpdateOrDeleteStatementImmidiately(createRemoveStatement(key));
 
 	}
@@ -160,8 +167,8 @@ public class AndroidAsyncMap<V> implements AsyncMap<String, V> {
 	@Override
 	public void start(SimpleCallback callback) {
 		if (injectedDb == null) {
-			db = SQLiteDatabase.openOrCreateDatabase(new File(conf.getDatabasePath()),
-					null);
+			db = SQLiteDatabase.openOrCreateDatabase(
+					new File(conf.getDatabasePath()), null);
 		} else {
 			db = injectedDb;
 		}
